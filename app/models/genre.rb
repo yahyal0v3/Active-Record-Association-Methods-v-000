2ac95d07881d7401ba482a3e6d759a2505620d97
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 20:00:11 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,7 +9,7 @@ class Genre < ActiveRecord::Base
 
   def artist_count
     # return the number of artists associated with the genre
-    self.artists.size 
+    self.artists.size
   end
 
   def all_artist_names
