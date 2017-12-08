ece8ef9c540f19e63b0c28a3474564480c848d1f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 20:05:03 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,7 +14,6 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    binding.pry
-    self.artists.name
+    self.artists.collect {|a| a.name}
   end
 end
